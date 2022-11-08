5fd7394bb4453c9b8396104ac1098f9ac0e1ab66
Connor Tree <Connor>
2022-11-08 12:04:12 +1030

    All data is now in order of ID.

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
 //  https://botw-compendium.herokuapp.com/api/v2
 //  API ^^^^^
+const objectListEl = document.querySelector(".object-list");
+
+
+async function main() {
+  const objects = await fetch("https://botw-compendium.herokuapp.com/api/v2")
+  const objectsData = await objects.json();
+  const dataArr = pushDataToArr(objectsData.data);
+
+  //Variable with sorted array
+  const sortedData = dataArr.sort((a,z)=> a.id - z.id);
+console.log(sortedData)
+  //Logic that turns sorted array into html
+  objectListEl.innerHTML = (sortedData.map(data => objectHTML(data)).join(""));
+  
+  console.log("This ran 3")
+  return dataArr.sort((a, z) => a.id - z.id);
+
+}
+
+main();
+
+
+//checking if the class is an array. If it is, it stores in arrStore, if not it turns the object into an array, then stores it.
+function pushDataToArr(data, arrStore = []) {
+  for (const category in data) {
+    if (data[category] instanceof Array) {
+      for (let i = 0; i < data[category].length; i++){
+        arrStore.push(data[category][i]);
+      }
+    }
+    else if (data[category] instanceof Object){
+      pushDataToArr(data[category], arrStore);
+    }
+  }
+  return arrStore;
+}
+
+
+function objectHTML(data){
+  console.log("this ran 2")
+  return `<div class="object-card">
+    <div class="object-card__container">
+
+      <h3 class="object__name">${data.name}</h3>
+      <img class="object__img" src="${data.image}">
+      <p><b class="object__headers">Category: </b>${data.category}</p>
+      <p><b class="object__headers">Common Locations: </b>${data.common_locations}</p>
+      <p><b class="object__headers">Descrption: </b>${data.description}</p>
+
+      <p><b class="object__headers">Cooking Effect: </b>${data.cooking_effect}</p>
+      <p><b class="object__headers">Hearts Recovered: </b>${data.hearts_recovered}</p>
+
+      <p><b class="object__headers">Drops: </b>${data.drops}</p>
+
+      <p><b class="object__headers">Attack: </b>${data.attack}</p>
+      <p><b class="object__headers">Defence: </b>${data.defense}</p>
+    </div>
+  </div>`;
+}
